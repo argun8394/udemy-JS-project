2d264aeba3682c0fe6d6a1713921b7f2489e520f
argun8394 <argundoganali@gmail.com>
2021-09-04 23:00:19 +0300

    slider

--- slider/script.js
+++ slider/script.js
@@ -16,11 +16,37 @@ var models=[
     link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'}
 ];
 
-var index =3;
+var index =0;
 
-document.querySelector(".card-title").textContent =models[index].name;
+var slaytCount=models.length;
+showSlide(index);
 
-document.querySelector(".card-img-top").setAttribute("src",models[index].image);
+document.querySelector(".fa-arrow-circle-left").addEventListener("click",function() {
+    index--;
+    showSlide(index);
+    console.log(index);
+});
 
-document.querySelector(".card-link").setAttribute("href",models[index].link);
+document.querySelector(".fa-arrow-circle-right").addEventListener("click",function() {
+    index++;
+    showSlide(index);
+    console.log(index);
+});
+
+function showSlide(i) {
+
+    index = i;
+    if (i<0){
+        index=slaytCount-1;
+    }
+    if(i>=slaytCount){
+        index=0;
+    }
+    document.querySelector(".card-title").textContent =models[index].name;
+
+    document.querySelector(".card-img-top").setAttribute("src",models[index].image);
+    
+    document.querySelector(".card-link").setAttribute("href",models[index].link);
+    
+}
 
