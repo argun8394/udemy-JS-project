eac481878327ff710f937ac852f697f0d376c5a5
argun8394 <argundoganali@gmail.com>
2021-09-04 22:43:12 +0300

    slider

---
+++ slider/script.js
@@ -0,0 +1,26 @@
+var models=[
+    {name: 'Bmw 418d',
+    image: './img/bmw.jpg',
+    link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'},
+    {name: 'Mazda CX-3',
+    image: './img/bugatti.jpg',
+    link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'},
+    {name: 'Volvo S60',
+    image: './img/lambo.jpg',
+    link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'},
+    {name: 'Skoda Superb',
+    image: './img/lamboyello(1).jpg',
+    link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'},
+    {name: 'Honda Civic',
+    image: './img/merc.jpg',
+    link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'}
+];
+
+var index =3;
+
+document.querySelector(".card-title").textContent =models[index].name;
+
+document.querySelector(".card-img-top").setAttribute("src",models[index].image);
+
+document.querySelector(".card-link").setAttribute("href",models[index].link);
+
