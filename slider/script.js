ca40c16e58441dc19a3218bcc8356dab73923de4
argun8394 <argundoganali@gmail.com>
2021-09-05 00:05:39 +0300

    slider

--- slider/script.js
+++ slider/script.js
@@ -16,37 +16,85 @@ var models=[
     link: 'http://www.arabalar.com.tr/bmw/4-serisi/2018/418d-2-0-gran-coupe'}
 ];
 
-var index =0;
+var index = 0;
+var slaytCount = models.length;
+var interval;
 
-var slaytCount=models.length;
-showSlide(index);
+var settings={
+    duration : '1000',
+    random : false
+};
 
-document.querySelector(".fa-arrow-circle-left").addEventListener("click",function() {
+init(settings);
+
+document.querySelector('.fa-arrow-circle-left').addEventListener('click',function(){
     index--;
     showSlide(index);
     console.log(index);
 });
 
-document.querySelector(".fa-arrow-circle-right").addEventListener("click",function() {
+document.querySelector('.fa-arrow-circle-right').addEventListener('click',function(){
     index++;
     showSlide(index);
-    console.log(index);
+    console.log(index);    
 });
 
-function showSlide(i) {
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseenter',function(){
+        clearInterval(interval);
+    })
+});
 
-    index = i;
-    if (i<0){
-        index=slaytCount-1;
-    }
-    if(i>=slaytCount){
-        index=0;
-    }
-    document.querySelector(".card-title").textContent =models[index].name;
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseleave',function(){
+        init(settings);
+    })
+})
 
-    document.querySelector(".card-img-top").setAttribute("src",models[index].image);
-    
-    document.querySelector(".card-link").setAttribute("href",models[index].link);
-    
+
+function init(settings){
+
+    var prev;
+    interval=setInterval(function(){
+        
+        if(settings.random){
+            // random index
+            do{
+                index = Math.floor(Math.random() * slaytCount);
+            }while(index == prev)
+            prev = index;
+        }else{
+            // artan index
+            if(slaytCount == index+1){
+                index = -1;
+            }
+            showSlide(index);
+            console.log(index);
+            index++;
+        }
+        showSlide(index);
+
+    },settings.duration)
+  
+
 }
 
+
+
+function showSlide(i){
+
+    index = i;
+
+    if (i<0) {
+        index = slaytCount - 1;
+    }
+    if(i >= slaytCount){
+        index =0;
+    }
+
+    document.querySelector('.card-title').textContent = models[index].name;
+
+    document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+    
+    document.querySelector('.card-link').setAttribute('href',models[index].link);
+}
